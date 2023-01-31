c5813978e537cb7f95f60064700c89552236c538
ameer yaesh <ameer1momo1@gmail.com>
2023-01-31 13:08:26 +0200

    first commit

---
+++ Task/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Task;
+
+bool flag = true;
+double num1 = 0;
+double num2 = 0;
+char op = '\0';
+double result = 0;
+bool yes = false;
+while (flag)
+{
+    try
+    {
+        
+        if (yes)
+        {
+            num1 = result;
+            yes = false;
+        }
+        else {
+            Console.WriteLine("Welcome to calculator \n Please enter the first number :");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+        }
+        Console.WriteLine(" \n Please enter the secound number :");
+        num2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine(" \n Please select  operation from (+,-,*,/) :");
+        string operation = Console.ReadLine();
+        //check if the user enter more than on operation
+        if(operation.Length>1)
+        {
+            throw new Exception("please choose one operation");
+        }
+        else
+        {
+            op = Convert.ToChar(operation);
+            //calculate the result of the two number based on which operation used
+            result= Calculator.Calculate(num1, num2, op);
+            Console.WriteLine($"result is:{result}");
+            Console.WriteLine("choose from the following \n If you want to do a cumulative process on the result, enter (yes) \n If you want to do calculations on new numbers, enter (no) \n If you want to exit the program, enter (exit)\n");
+            string choice = Console.ReadLine().ToLower();
+            switch(choice)
+            {
+                case "yes":
+                    yes = true;
+                    break;
+                case "no":
+                     break;
+                case "exit":
+                    flag = false;
+                    break;
+                default:
+                    Console.WriteLine("the choice you enter is not valid");
+                    break;
+
+
+
+            }
+
+        }
+
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+        
+
+    }
+   
+}
---
+++ Task/calculator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    public class Calculator
+    {
+        //this function calculate the result of the operation that user choose on two numbers the user enter 
+        public static double Calculate(double firstNumber, double secondNumber, char opreator)
+        {
+
+            double result = 0;
+            try
+            {
+
+                //based on the operation the user enter the code will calculate
+                switch (opreator)
+                {
+                    case '+': result = firstNumber + secondNumber; break;
+                    case '-': result = firstNumber - secondNumber; break;
+                    case '*': result = firstNumber * secondNumber; break;
+                    case '/':
+                        if (secondNumber == 0)
+                        {
+                            throw new ArithmeticException("the secound number cant be 0");
+                        }
+                        else
+                        {
+                            result = firstNumber / secondNumber; break;
+                        }
+                    default:
+                        Console.WriteLine("Please choose from the opreation from the allowed list");
+                        result = 0; break;
+
+                }
+
+                return result;
+
+
+
+            }
+            catch (Exception e)
+
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+
+
+
+            }
+
+
+
+
+
+
+        }
+
+    }
+}
